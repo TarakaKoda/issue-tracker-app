e15109420e80d6c5f2a1505fd0a6339327329cec
TarakaKoda <taraka.koda@gmail.com>
2024-02-01 16:36:21 +0530

    Build an API for creating issue.

---
+++ app/api/issues/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Title must be at least 1 character." })
+    .max(255, { message: "Title cannot be more then 255 characters." }),
+  description: z
+    .string()
+    .min(1, { message: "Description must be at least 1 character." }),
+});
+
+export default createIssueSchema;
