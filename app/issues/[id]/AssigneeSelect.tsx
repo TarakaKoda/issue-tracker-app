a6224b57f9f41197fc3fbeed3886ea069c250c52
TarakaKoda <taraka.koda@gmail.com>
2024-02-09 23:03:19 +0530

    Assign issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -11,11 +11,11 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Issue } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { MdOutlineAssignmentInd } from "react-icons/md";
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -27,12 +27,21 @@ const AssigneeSelect = () => {
     retry: 3,
   });
 
-  if(error) return null;
+  if (error) return null;
 
-  if(isLoading) return <Skeleton className="w-[10rem] h-10 bg-[#f3f3f5] dark:bg-[#333]"/>
+  if (isLoading)
+    return <Skeleton className="h-10 w-[10rem] bg-[#f3f3f5] dark:bg-[#333]" />;
 
   return (
-    <Select>
+    <Select
+      defaultValue={issue.assignedToUserId || "unassigned"}
+      onValueChange={(userId) => {
+        const assignedUserId = userId === "unassigned" ? null : userId;
+        axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: assignedUserId,
+        });
+      }}
+    >
       <SelectTrigger className="dark:border-[#333] md:min-w-[10rem]">
         <MdOutlineAssignmentInd className="text-lg" />
         <SelectValue placeholder="Assign Issue" />
@@ -40,6 +49,7 @@ const AssigneeSelect = () => {
       <SelectContent className="bg-background dark:border-[#222]">
         <SelectGroup>
           <SelectLabel>Suggestions</SelectLabel>
+          <SelectItem value={"unassigned"}>Unassigned</SelectItem>
           {users?.map((user) => (
             <SelectItem key={user.id} value={user.id}>
               {user.name}
--- app/issues/[id]/IssueDeleteContainer.tsx
+++ app/issues/[id]/IssueDeleteContainer.tsx
@@ -6,7 +6,7 @@ import AssigneeSelect from "./AssigneeSelect";
 const IssueDeleteContainer = ({ issue }: { issue: Issue }) => {
   return (
     <div className="flex gap-3 max-sm:flex-col lg:max-w-36 lg:flex-col lg:justify-self-end lg:mr-4">
-      <AssigneeSelect />
+      <AssigneeSelect issue={issue} />
       <IssueEditButton issueId={issue.id} />
       <IssueDeleteAlertDialogue issue={issue} />
     </div>
